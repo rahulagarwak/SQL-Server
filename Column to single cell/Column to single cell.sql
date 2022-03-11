86be4d359158baa21fb994fc8431be7f52edafe7
Rahul Agarwal <ragwalmrcool@gmail.com>
2022-03-11 09:29:14 +0530

    Add files via upload

---
+++ Column to single cell/Column to single cell.sql
@@ -0,0 +1,25 @@
+Declare @name varchar(max);
+Declare @i varchar(max);
+
+create table #temp (Id int, CName Varchar(max))
+insert into #temp values (1,'India'), (2,'Australia'), (3,'Srilanka'), (4,'England')
+
+select * from #temp
+
+declare c cursor for 
+select CName from #temp
+
+open c
+fetch next from c into @i
+
+while @@FETCH_STATUS=0
+begin
+set @name = concat_ws(',',@name,@i)
+fetch next from c into @i
+end
+
+select @name
+
+CLOSE c
+DEALLOCATE c
+drop table #temp
